201d930a0f452231159e1688a2e0bb85f6a6905d
gazami <lin1137001@gmail.com>
2024-10-02 11:27:07 +0900

    2nd p5js

---
+++ Magnetic_camel_2024_10_02_02_11_18/Magnetic_camel_2024_10_02_02_25_44/sketch.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+  
+  
+}
+
+function draw() {
+  background(0,102,051);
+  //fill(255,0,0);
+  //noFill();
+  //noStroke();
+  strokeWeight(1);
+  fill(0,0,255);
+  stroke(255,0,0);
+  rect(10,10,100,100);
+  strokeWeight(10);
+  
+  fill(255,255,255);
+  stroke(0,0,0);
+  rect(110,110,100,100);
+  // stroke(0);
+  // line(10,10,100,100);
+  // stroke(150);
+  // line(100,100,200,100);
+  // stroke(255,255,0);
+  // line(200,100,200,200);
+  
+  //rect(100,100,200,200);
+//   ellipse(200,200,200,200);
+  
+//   //arc(200,200,200,200,PI, 3*PI);
+//   arc(160,170,20,20,PI,3*PI);
+//   arc(240,170,20,20,PI,3*PI);
+//   point(160,170);
+//   point(240,170);
+//   line(200,200,215,200);
+//   arc(200,250,20,20,2*PI,3*PI);
+  
+  
+  
+}
